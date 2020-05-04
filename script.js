e18dbd7dd17c8dc063fbb8f01336d7fd6aeabd54
Killian7575 <killian.adamowicz@gmail.com>
2020-05-04 15:04:05 +1000

    Updated to V4, Now using jQuery, Un-Hardcoded the grid, Added "use strict";

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-function initialNumbers() {                    // sets the initial problem
-    document.getElementById('A1').value = 1;   // Hard code
-    document.getElementById('A2').value = "";  // Hard code
-    document.getElementById('A3').value = 0;   // etc
-    document.getElementById('A4').value = "";
-    document.getElementById('A5').value = 0;
-    document.getElementById('A6').value = 0;
-    document.getElementById('B1').value = "";
-    document.getElementById('B2').value = 1;
-    document.getElementById('B3').value = "";
-    document.getElementById('B4').value = 0;
-    document.getElementById('B5').value = 0;
-    document.getElementById('B6').value = "";
-    document.getElementById('C1').value = "";
-    document.getElementById('C2').value = "";
-    document.getElementById('C3').value = "";
-    document.getElementById('C4').value = "";
-    document.getElementById('C5').value = "";
-    document.getElementById('C6').value = 0;
-    document.getElementById('D1').value = 0;
-    document.getElementById('D2').value = "";
-    document.getElementById('D3').value = "";
-    document.getElementById('D4').value = 1;
-    document.getElementById('D5').value = "";
-    document.getElementById('D6').value = "";
-    document.getElementById('E1').value = 1;
-    document.getElementById('E2').value = 0;
-    document.getElementById('E3').value = "";
-    document.getElementById('E4').value = 1;
-    document.getElementById('E5').value = 1;
-    document.getElementById('E6').value = "";
-    document.getElementById('F1').value = "";
-    document.getElementById('F2').value = 0;
-    document.getElementById('F3').value = "";
-    document.getElementById('F4').value = "";
-    document.getElementById('F5').value = 1;
-    document.getElementById('F6').value = 1;
+"use strict";
+// script.js V4
+// Setting constants 
+const G1 = [1, null, 0, null, 0, 0, null, 1, null, 0, 0, null, null, null, null, null, null, 0, 0, null, null, 1, null, null, 1, 0, null, 1, 1, null, null, 0, null, null, 1, 1];
+const G2 = [null, 1, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+const G3 = [null, null, 1, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+
+const row = 6;
+const column = 6;
+
+$("body").ready(function() {
+    loadGrid(G1);
+});
+
+function loadGrid(grid) {           
+    for (var i = 0; i < row; i++) {
+        $("#A" + (i + 1)).val(grid[i]);
+    }
+    for (var i = 0; i < row; i++) {
+        $("#B" + (i + 1)).val(grid[column + i]);
+    }
+    for (var i = 0; i < row; i++) {
+        $("#C" + (i + 1)).val(grid[column * 2 + i]);
+    }
+    for (var i = 0; i < row; i++) {
+        $("#D" + (i + 1)).val(grid[column * 3 + i]);
+    }
+    for (var i = 0; i < row; i++) {
+        $("#E" + (i + 1)).val(grid[column * 4 + i]);
+    }
+    for (var i = 0; i < row; i++) {
+        $("#F" + (i + 1)).val(grid[column * 5 + i]);
+    }
 };
