89d9a0d59abf8787246781666ee146d389d49a05
Killian7575 <killian.adamowicz@gmail.com>
2020-05-12 12:09:12 +1000

    V6, added debug boxes under grid that hold the in text form

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 'use strict';
-// script.js V4
+// script.js V06
 // Setting constants 
 const G1 = [1, null, 0, null, 0, 0, null, 1, null, 0, 0, null, null, null, null, null, null, 0, 0, null, null, 1, null, null, 1, 0, null, 1, 1, null, null, 0, null, null, 1, 1];
 const G2 = [null, 1, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
 const G3 = [null, null, 1, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+const gridtest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
 
 const row = 6;
 const column = 6;
 
 $('body').ready(function() {
-    loadGrid(G1);
+    loadGrid(gridtest);
 });
 
 function loadGrid(grid) {
+    loadArrayViews(grid);
     for (var i = 0; i < row; i++) {
         for (var y = 0; y < column; y++) {
             $('#B' + (i + 1) + (y + 1)).val(grid[(i * column) + y])
@@ -20,23 +22,19 @@ function loadGrid(grid) {
     }
 };
 
-// function loadGrid(grid) {           
-//     for (var i = 0; i < row; i++) {
-//         $("#A" + (i + 1)).val(grid[i]);
-//     }
-//     for (var i = 0; i < row; i++) {
-//         $("#B" + (i + 1)).val(grid[column + i]);
-//     }
-//     for (var i = 0; i < row; i++) {
-//         $("#C" + (i + 1)).val(grid[column * 2 + i]);
-//     }
-//     for (var i = 0; i < row; i++) {
-//         $("#D" + (i + 1)).val(grid[column * 3 + i]);
-//     }
-//     for (var i = 0; i < row; i++) {
-//         $("#E" + (i + 1)).val(grid[column * 4 + i]);
-//     }
-//     for (var i = 0; i < row; i++) {
-//         $("#F" + (i + 1)).val(grid[column * 5 + i]);
-//     }
-// };
+function loadArrayViews(grid) {
+    var tempArray = [];
+    var finalArray = [];
+    for (var i = 0; i < row; i++) {
+        for (var y = 0; y < column; y++) {
+            tempArray.push(grid[(i * column) + y]);
+        }
+        finalArray.push(tempArray);
+        tempArray = [];
+    }
+    for (var i = 0; i < row; i++) {
+        $('#2DArrayBox').html($('#2DArrayBox').html() + finalArray[i]);
+        $('#2DArrayBox').html($('#2DArrayBox').html() + '<br>');
+    }
+    $('#2DArraySingle').html(finalArray[2][4]);
+};
